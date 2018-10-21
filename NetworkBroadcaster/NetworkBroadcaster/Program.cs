01c618a4a2d35a62fb6707840c1d78b5ffda2f0e
Rakesh P <FeelCoffee@hotmail.com>
2018-10-21 19:43:30 -0400

    Network Broadcaster: Initial

---
+++ NetworkBroadcaster/NetworkBroadcaster/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkBroadcaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PORT = 4000;
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            
+            //var from = new IPEndPoint(0, 0);
+            //Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        var recvBuffer = udpClient.Receive(ref from);
+            //        Console.WriteLine(Encoding.UTF8.GetString(recvBuffer));
+            //    }
+            //});
+
+            var data = Encoding.UTF8.GetBytes("ABCD");
+            udpClient.Send(data, data.Length, "255.255.255.255", PORT);
+        }
+
+
+    }
+}
