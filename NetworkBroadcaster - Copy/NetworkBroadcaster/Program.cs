91ee21dce0ed123bdac72f9118b9d128a97d1490
Rakesh P <FeelCoffee@hotmail.com>
2018-10-21 22:12:38 -0400

    Copy To Test

---
+++ NetworkBroadcaster - Copy/NetworkBroadcaster/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkBroadcaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<byte> msg = AdsbHeartbeat.Create();
+
+            int PORT = 4000;
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            
+            //var from = new IPEndPoint(0, 0);
+            //Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        var recvBuffer = udpClient.Receive(ref from);
+            //        Console.WriteLine(Encoding.UTF8.GetString(recvBuffer));
+            //    }
+            //});
+
+            var data = Encoding.UTF8.GetBytes("VIDHI");
+            udpClient.Send(data, data.Length, "255.255.255.255", PORT);
+        }
+
+
+    }
+}
